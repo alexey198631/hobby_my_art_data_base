41d801c35e7a385ddacb7131b4d7f5ff4c77b873
Alex <al.an.gukov@gmail.com>
2023-01-14 16:57:18 +0100

    workable version with json file

--- art_import.py
+++ art_import.py
@@ -1,4 +1,10 @@
+"""
+['START', 'DATE', 'DAYS', 'TYPE', 'AWARD', 'LANG', 'TIMES', 'NAME',
+       'YEAR', 'FACT_DURATION', 'ACTIVITY', 'DURATION', 'CREATOR', 'WHERE',
+       'COUNTRY', 'PTS', 'COMMENTS']
+"""
+
 import pandas as pd
 
-total = pd.read_excel('data_files/Days.xlsx', sheet_name='art')
-
+art = pd.read_excel('data_files/Days.xlsx', sheet_name='art')
+art_test = art.dropna(subset='DATE')
